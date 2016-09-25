d4f1bae808f595d5478ae036557c7a1062dac372
Robin Feng <arthurkkk@sina.com>
2016-09-25 11:14:51 +0800

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,14 +18,28 @@ class NewVisitorTest(unittest.TestCase):
 
 		# 她注意到网页的标题和头部都包含"待办"这个词
 		self.assertIn('待办', self.browser.title)
-		self.fail('测试完成!')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('待办',header_text)
 
 		# 应用邀请她输入一个待办事项
+		inputbox = self.browser.find_element_by_tag_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'输入一个待办事项'
+		)
 		# 她在一个文本框中输入了"购买孔雀羽毛"
 		# 晨晨的爱好是使用假蝇做饵钓鱼
+		inputbox.send_keys('购买孔雀羽毛')
 
 		# 她按回车键后，页面更新了
 		# 待办事项表格中显示了"1. 购买孔雀羽毛"
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_tag_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: 购买孔雀羽毛' for row in rows)
+		)
 
 		# 页面中又显示了一个文本框，可以输入其他的待办事项
 		# 她输入了"使用孔雀羽毛做假蝇"
@@ -39,6 +54,7 @@ class NewVisitorTest(unittest.TestCase):
 		# 她访问那个URL，发现她的待办事项列表还在
 
 		# 她很满意，去睡觉了
+		self.fail('测试完成!')
 
 if __name__ == '__main__':
 #	unittest.main(warnings='ignore')
